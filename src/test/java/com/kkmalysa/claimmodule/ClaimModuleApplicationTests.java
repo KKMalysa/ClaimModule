933284d56faba0083c8efd73d451184896bad6ae
karol <k.malysa010@gmail.com>
2025-01-27 20:09:57 +0100

    initial

---
+++ src/test/java/com/kkmalysa/claimmodule/ClaimModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkmalysa.claimmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
