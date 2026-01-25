dd5343dea5940947cd9c29bcb971c2bd523be7f5
KKMalysa <k.malysa010@gmail.com>
2026-01-25 23:22:16 +0100

    Initial project setup (Maven, builder, claim module)

---
+++ src/main/java/com/kkmalysa/claimmodule/ClaimModuleApplication.java
@@ -0,0 +1,13 @@
+package com.kkmalysa.claimmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimModuleApplication.class, args);
+    }
+
+}
